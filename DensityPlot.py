970b248b98077ce24494267025a88fdcc3ea474b
Admiralhunter <hunterpalcich18@gmail.com>
2018-12-15 22:25:05 -0500

    Add files via upload

---
+++ DensityPlot.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot as plt
+import csv
+from scipy.stats import kde
+import numpy as np
+
+#ensure the file is located in the same folder as the D
+str = 'Week 12 Trial Runs.csv'
+
+counter = -1
+x = []
+y = []
+with open(str) as csvDataFile:
+
+    csvReader = csv.reader(csvDataFile)
+    for row in csvReader:
+        counter = counter + 1
+        if counter > 0:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+
+
+
+min = -5
+max = 5
+
+nbins = 100
+k = kde.gaussian_kde([x,y])
+xi, yi = np.mgrid[min:max:nbins*1j, min:max:nbins*1j]
+zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+plt.pcolormesh(xi, yi, zi.reshape(xi.shape))
+plt.colorbar()
+plt.xlim(min,max)
+plt.ylim(min,max)
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.title('Heat Map Week 12')
+plt.show()
